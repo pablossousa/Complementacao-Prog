ee71ea5c3166b1e0720bd78a2cdf6fcee9dc0844
Pablo Sousa <pablossilva.005@gmail.com>
2025-01-15 00:40:33 -0300

    Update estrutura.h

--- src/estrutura.h
+++ src/estrutura.h
@@ -5,21 +5,22 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <limits.h>
 
 #define NUM_CAIXAS 5
 
 typedef struct Cliente {
     char nome[100];
     char cpf[12];
-    int prioridade;  // 1: Alta, 2: Média, 3: Baixa
+    int prioridade;
     int itens;
     struct Cliente *proximo;
 } Cliente;
 
 typedef struct Caixa {
     int id;
-    bool aberto;  // true: aberto, false: fechado
-    Cliente *fila;  // Ponteiro para o primeiro cliente da fila
+    bool aberto;
+    Cliente *fila;  
 } Caixa;
 
 void inicializarCaixas(Caixa caixas[NUM_CAIXAS]);
